702316dd79f3dcc460a6f8b62b263d0d64bd3557
Kim Anh <anhnguyen.88234020333@st.ueh.edu.vn>
2026-04-23 11:06:20 +0700

    Kim Anh: Hoàn thành kết nối Database và cấu hình DbContext.

---
+++ PetLuv/Data/PetLuvDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PetLuv.Data
+{
+    public class PetLuvDbContext : DbContext
+    {
+        public PetLuvDbContext(DbContextOptions<PetLuvDbContext> options) : base(options) { }
+
+        // để trống xong Model thêm vào sau
+    }
+
+}
--- PetLuv/Program.cs
+++ PetLuv/Program.cs
@@ -1,7 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Đăng ký kết nối Database
+builder.Services.AddDbContext<PetLuv.Data.PetLuvDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
